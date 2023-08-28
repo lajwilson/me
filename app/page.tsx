9010feb5868965df942e9e318876ddd72f961490
Lloyd Wilson <lajwilson77@gmail.com>
2023-08-28 15:24:39 -0400

    Initial code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1 className="text-3xl">Lloyd Wilson</h1>
+      <h1 className="text-3xl font-thin">Lloyd Wilson</h1>
     </div>
   );
 }
