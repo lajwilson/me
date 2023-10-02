99da68e42b654693d3209a0790d66b2a0053aace
Lloyd Wilson <lloyd.wilson@avaropoint.com>
2023-10-02 16:44:51 -0400

    Minor code clean up

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/admin/",
+      disallow: ["/admin/", "/api/", "/_next/", "/auth/"],
     },
     sitemap: "https://lloydwilson.me/sitemap.xml",
   };
